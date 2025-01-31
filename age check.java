003b66edb098f20350aacb383b998a01f9cf9e4a
rolex1938 <rolex193814@gmail.com>
2025-02-01 00:19:58 +0530

    Update agecheck.java

--- age check.java
+++ age check.java
@@ -1,1 +1,11 @@
 package ram;
+
+class main{
+   public static void main(string [] args) {
+       int age=18;
+       if(age>=18)
+       {
+            System.out.print("eligible");
+       }
+   }
+}
